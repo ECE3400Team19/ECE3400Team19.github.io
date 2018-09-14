db54b4543aba08f45cd386cfc338b58e735147b1
asenaulug <42787268+asenaulug@users.noreply.github.com>
2018-09-13 21:57:20 -0400

    switched sensors to the front

--- Milestone1/milestone1.ino
+++ Milestone1/milestone1.ino
@@ -1,30 +1,63 @@
 #include <Servo.h>
 
+Servo left; //180 turns wheel backward
+Servo right; //180 turns wheel forward
 
-//int pot = A0;
-Servo left; //180 turns wheel forward
-Servo right; //180 turns wheel backward
-//int line[3];
-int leftPin = A5; //left
-int middlePin = A1; //center
-int rightPin = A0; //right
+int leftPin = A0; //left
+int middlePin = A5; //center
+int rightPin = A1; //right
 int leftSensor;
 int rightSensor;
 int middleSensor;
 int leftSpeed;
 int rightSpeed;
+int numIntersections = 0;
 
 
 void setup() {
   Serial.begin(9600);
-  left.attach(5);
-  right.attach(10);
-  //pinMode(line2, INPUT);
+  left.attach(10);
+  right.attach(5);
+
+//  //turn left
+//  leftSpeed = 95;
+//  rightSpeed = 95;
+//  left.write(95);
+//  right.write(95);
+
+//  //turn right
+//  leftSpeed = 85;
+//  rightSpeed = 85;
+//  left.write(85);
+//  right.write(85);
+
+// //go forward
+// left.write(95)
+// right.write(85)
   
+
+
 }
 
 void loop() {
-  followLine();
+  //followLine();
+  figureEight();
+
+
+
+  
+  
+//    leftSensor = analogRead(leftPin);
+//    middleSensor = analogRead(middlePin);
+//    rightSensor = analogRead(rightPin);
+//     Serial.print("Left: ");
+//     Serial.print(leftSensor);
+//     Serial.print(" Middle: ");
+//     Serial.print(middleSensor);
+//     Serial.print(" Right: ");
+//     Serial.print(rightSensor);
+//     Serial.println();
+
 }
 
 void followLine(){
@@ -34,16 +67,16 @@ void followLine(){
     rightSensor = analogRead(rightPin);
 
 
-      if (leftSensor < 300 && middleSensor< 300 && rightSensor < 300){
+      if (leftSensor < 800  && middleSensor< 800 && rightSensor < 800){
         // reaches the INTERSECTION
         leftSpeed = 85;
         rightSpeed =85;
         left.write(leftSpeed);
         right.write(rightSpeed);
         delay(400);
-//        while( leftSensor > 300);
+//        while( leftSensor > 800);
       }
-      else if ( middleSensor < 300){
+      else if ( middleSensor < 800){
         //go straight!!
         leftSpeed = 95;
         rightSpeed = 85;
@@ -51,12 +84,12 @@ void followLine(){
         right.write(rightSpeed);
       }
       else {
-        if (leftSensor < 300){
+        if (leftSensor < 800){
           //TURN LEFT
           leftSpeed = 85;
           rightSpeed = 85;
         }
-        if (rightSensor < 300){
+        if (rightSensor < 800){
           //TURN RIGHT
           leftSpeed = 95;
           rightSpeed = 95;
@@ -77,29 +110,39 @@ void followLine(){
 }
 
 void figureEight(){
-    int numIntersections;
+    
     leftSensor = analogRead(leftPin);
     middleSensor = analogRead(middlePin);
     rightSensor = analogRead(rightPin);
 
 
-      if (leftSensor < 300 && middleSensor< 300 && rightSensor < 300){
+      if (leftSensor < 800 && middleSensor< 800 && rightSensor < 800){
         // reaches the INTERSECTION
-        numIntersections++;
-        if (numIntersections>4){
+        if (numIntersections == 8) {numIntersections = 0;}
+        if (numIntersections>3){
+            //turn left
+            Serial.println(numIntersections);
+            //++numIntersections;
             leftSpeed = 85;
             rightSpeed = 85;
         }
         else{
+          //turn right
+          Serial.println(numIntersections);
+          //++numIntersections;
           leftSpeed = 95;
           rightSpeed = 95;
         }
+        ++numIntersections;
+        left.write(95);
+        right.write(85);
+        delay(200);
         left.write(leftSpeed);
         right.write(rightSpeed);
-        delay(400);
-        //while( leftSensor > 300);
+        delay(700);
+        //while( leftSensor > 800);
       }
-      else if ( middleSensor < 300){
+      else if ( middleSensor < 800){
         //go straight!!
         leftSpeed = 95;
         rightSpeed = 85;
@@ -107,12 +150,12 @@ void figureEight(){
         right.write(rightSpeed);
       }
       else {
-        if (leftSensor < 300){
+        if (leftSensor < 800){
           //TURN LEFT
           leftSpeed = 85;
           rightSpeed = 85;
         }
-        if (rightSensor < 300){
+        if (rightSensor < 800){
           //TURN RIGHT
           leftSpeed = 95;
           rightSpeed = 95;
