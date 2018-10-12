27c477c5f9f602eb6299c2e81d1b41609557a319
Robert <rjm448@cornell.edu>
2018-10-12 15:43:33 -0400

    Milestone 2 Code

--- Milestone2/distanceDetection/distanceDetection.ino
+++ Milestone2/distanceDetection/distanceDetection.ino
@@ -1,4 +1,8 @@
 #include <Servo.h>
+#define LOG_OUT 1 // use the log output function
+#define FFT_N 256 // set to 256 point fft
+
+#include <FFT.h> // include the library
 
 Servo left; //180 turns wheel backward
 Servo right; //180 turns wheel forward
@@ -12,150 +16,34 @@ int rightSensor;
 int middleSensor;
 int leftSpeed;
 int rightSpeed;
-float dis=0;
-boolean detected = false;
+int lw = 6;
+int fw = 7;
+int rw = 8;
+boolean IRDetected;
 
-
-void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
-  left.attach(10);
-  right.attach(5);
-  float sensorValue = analogRead(A2); // Read sensor value for analog pin A2
-  float dis = 2076/(sensorValue - 11); // convert to a distance
-  while (1){
-    float sensorValue = analogRead(A2); // Read sensor value for analog pin A2
-    float dis = 2076/(sensorValue - 11); // convert to a distance
-    if (dis < 20) break;
-    Serial.println("looking for wall");
-    //delay(300);
-    Serial.println(dis);
-    left.write(95);
-    right.write(90);
-    delay(100);
-  }
-  
-}
-
-void loop() {
-
-    float sensorValue = analogRead(A2); // Read sensor value for analog pin A2
-    float dis = 2076/(sensorValue - 11); // convert to a distance
-
-    //read line sensors
-    leftSensor = analogRead(leftPin);
-    middleSensor = analogRead(middlePin);
-    rightSensor = analogRead(rightPin);    
-
-    
-    //At Intersection, need to decide where to go
-    if (leftSensor < 800  && middleSensor< 800 && rightSensor < 800){
-        //FFT IR
-        //code here
-    
-        //left wall
-        //code here
-    
-        //front wall
-        //code here
-    
-        //right wall
-        //code here
-    
-        //What can you see?
-        int seen = 0'b0000
-        if (IR detected)  seen |= 0'b1000;
-        if (leftWall)     seen |= 0'b0100;
-        if (frontWall)    seen |= 0'b0010;
-        if (rightWall)    seen |= 0'b0001;
-        
-        switch (seen) {
-          case 0'b0110:
-            //wall in front and left, turn right
-            turnRight()
-            break;
-          case 0'b0011:
-            //wall in front and right, turn left
-            turnLeft();
-            break;
-          case 0'b0111:
-            //wall in front, right and left, turn around
-            turnAround();
-            break;
-          case 0'b1100:
-            //wall on left, robot in front, turn right
-            turnRight()
-            break;
-          case 0'b1001:
-            //wall on right, robot in front, turn left
-            turnLeft();
-            break;
-          case 0'b1101:
-            //wall on right and left, robot in front, turn around
-            turnAround();
-            break;
-          //weird cases
-          case 0'b1110:
-            //wall on left and front, robot in front?, turn right
-            turnRight()
-            break;
-          case 0'b1011:
-            //wall on right and front, robot in front?, turn left
-            turnLeft();
-            break;
-          case 0'b1111:
-            //wall on right and left and front, robot in front?, turn around
-            turnAround();
-            break;
-          default:
-            //go straight
-            goStraight()
-            break;
+//helper methods
+void runFFT(){
+    cli();  // UDRE interrupt slows this way down on arduino1.0
+    for (int i = 0 ; i < 512 ; i += 2) { // save 256 samples
+      while(!(ADCSRA & 0x10)); // wait for adc to be ready
+      ADCSRA = 0xf5; // restart adc
+      byte m = ADCL; // fetch adc data
+      byte j = ADCH;
+      int k = (j << 8) | m; // form into an int
+      k -= 0x0200; // form into a signed int
+      k <<= 6; // form into a 16b signed int
+      fft_input[i] = k; // put real data into even bins
+      fft_input[i+1] = 0; // set odd bins to 0
     }
-    //straight line following
-    else if ( middleSensor < 800){
-        //go straight!!
-        leftSpeed = 95;
-        rightSpeed = 85;
-        left.write(leftSpeed);
-        right.write(rightSpeed);
-      }
-      else {
-        if (leftSensor < 800){
-          //TURN LEFT
-          leftSpeed = 85;
-          rightSpeed = 85;
-        }
-        if (rightSensor < 800){
-          //TURN RIGHT
-          leftSpeed = 95;
-          rightSpeed = 95;
-        }
-        left.write(leftSpeed);
-        right.write(rightSpeed);
-      }
-
-    
-    if (dis < 4 || dis >30) {
-        Serial.print( "Out of Range.\n");
+    fft_window(); // window the data for better frequency response
+    fft_reorder(); // reorder the data before doing the fft
+    fft_run(); // process the data in the fft
+    fft_mag_log(); // take the output of the fft
+    sei();
+    Serial.println("start");
+    for (byte i = 0 ; i < FFT_N/2 ; i++) { 
+      Serial.println(fft_log_out[i]); // send out the data
     }
-    else {
-        Serial.print(dis);
-        Serial.print(" cm\n"); 
-        
-    }
-    if (dis < 10){
-      //wall is to the left, go forward
-       left.write(95);
-       right.write(85);
-    }
-    else{
-        //turn left
-      left.write(85);
-      right.write(85);
-      delay(400);
-    }
-    delay(500);
 }
 
 void goStraight() {
@@ -167,34 +55,128 @@ void goStraight() {
 }
 
 void turnLeft() {
-  
+  leftSpeed = 85;
+  rightSpeed = 85;
+  left.write(95);
+  right.write(85);
+  delay(200);
+  left.write(leftSpeed);
+  right.write(rightSpeed);
+  delay(700);
 }
 
 void turnRight() {
-  
+  leftSpeed = 95;
+  rightSpeed = 95;
+  left.write(95);
+  right.write(85);
+  delay(200);
+  left.write(leftSpeed);
+  right.write(rightSpeed);
+  delay(700);
 }
 
 void turnAround() {
-  
+ leftSpeed = 95;
+  rightSpeed = 95;
+  left.write(95);
+  right.write(85);
+  delay(200);
+  left.write(leftSpeed);
+  right.write(rightSpeed);
+  delay(1400); 
 }
 
-void followLine(){
-  
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200); // use the serial port
+  left.attach(10);
+  right.attach(5);
+  pinMode(lw, INPUT);
+  pinMode(fw, INPUT);
+  pinMode(rw, INPUT);
+  //fft
+  TIMSK0 = 0; // turn off timer0 for lower jitter
+  ADCSRA = 0xe5; // set the adc to free running mode
+  ADMUX = 0x43; // use adc0
+  DIDR0 = 0x01; // turn off the digital input for adc0
+}
+
+void loop() {
+
+    //read line sensors
     leftSensor = analogRead(leftPin);
     middleSensor = analogRead(middlePin);
-    rightSensor = analogRead(rightPin);
+    rightSensor = analogRead(rightPin);    
 
-
-      if (leftSensor < 800  && middleSensor< 800 && rightSensor < 800){
-        // reaches the INTERSECTION
-        leftSpeed = 85;
-        rightSpeed =85;
-        left.write(leftSpeed);
-        right.write(rightSpeed);
-        delay(400);
-//        while( leftSensor > 800);
-      }
-      else if ( middleSensor < 800){
+    
+    //At Intersection, need to decide where to go
+    if (leftSensor < 800  && middleSensor< 800 && rightSensor < 800){
+        //FFT IR
+        ADMUX = 0x43;
+        runFFT();
+        for (int i = 38; i < 48; i++){
+          if (fft_log_out[i] > 100){
+            IRDetected = 1;
+            break;
+          }
+        }
+        
+        //What can you see?
+        int seen = B0000;
+        if (IRDetected)         seen |= B1000;
+        //left wall
+        if (digitalRead(lw))    seen |= B0100;
+        //front wall
+        if (digitalRead(fw))    seen |= B0010;
+        //right wall
+        if (digitalRead(rw))    seen |= B0001;
+        
+        switch (seen) {
+          case B0110:
+            //wall in front and left, turn right
+            turnRight();
+            break;
+          case B0011:
+            //wall in front and right, turn left
+            turnLeft();
+            break;
+          case B0111:
+            //wall in front, right and left, turn around
+            turnAround();
+            break;
+          case B1100:
+            //wall on left, robot in front, turn right
+            turnRight();
+            break;
+          case B1001:
+            //wall on right, robot in front, turn left
+            turnLeft();
+            break;
+          case B1101:
+            //wall on right and left, robot in front, turn around
+            turnAround();
+            break;
+          //weird cases
+          case B1110:
+            //wall on left and front, robot in front?, turn right
+            turnRight();
+            break;
+          case B1011:
+            //wall on right and front, robot in front?, turn left
+            turnLeft();
+            break;
+          case B1111:
+            //wall on right and left and front, robot in front?, turn around
+            turnAround();
+            break;
+          default:
+            //go straight
+            goStraight();
+            break;
+    }
+    //straight line following
+    }else if ( middleSensor < 800){
         //go straight!!
         leftSpeed = 95;
         rightSpeed = 85;
@@ -215,14 +197,5 @@ void followLine(){
         left.write(leftSpeed);
         right.write(rightSpeed);
       }
-      
-     
-     //writing 100 to both turns it right
-     Serial.print("Left: ");
-     Serial.print(leftSensor);
-     Serial.print(" Middle: ");
-     Serial.print(middleSensor);
-     Serial.print(" Right: ");
-     Serial.print(rightSensor);
-     Serial.println();
+
 }
